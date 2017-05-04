acf997b648d06f660e10531253b92cd8a9e71268
zhangzheng <zhangzheng@qianbao.com>
2017-05-04 03:49:10 +0000

    下载七牛云文件时，避免使用缓存

--- quick_start.sh
+++ quick_start.sh
@@ -9,11 +9,11 @@ fi
 
 cd bin
 if [ ! -f "qshell" ]; then
-  wget http://oerp142a4.bkt.clouddn.com/qshell
+  wget http://oerp142a4.bkt.clouddn.com/qshell?q=$RANDOM -O qshell
   chmod +x qshell
 fi
 
 if [ ! -f "zshell" ]; then
-  wget http://oerp142a4.bkt.clouddn.com/zshell
+  wget http://oerp142a4.bkt.clouddn.com/zshell?q=$RANDOM -O zshell
   chmod +x zshell
 fi
